b46562c5f6e238232441baedb82081df61394a7d
Sergio Lopez <slp@redhat.com>
2019-05-31 10:16:44 +0000

    Implement support for async storage backends

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@
 
 use std::collections::HashMap;
 use std::mem;
-use std::os::unix::io::AsRawFd;
+use std::os::unix::io::{AsRawFd, RawFd};
 use std::process::exit;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time::Instant;
 
 use clap::{crate_authors, crate_version, App, Arg};
 use log::{debug, error, info};
+use vhost_user_blk::backend::StorageBackend;
 use vhost_user_blk::backend_raw::StorageBackendRaw;
+use vhost_user_blk::backend_raw_async::StorageBackendRawAsync;
 use vhost_user_blk::block::{VhostUserBlk, Vring};
 use vhost_user_blk::eventfd::EventFd;
 use vhost_user_blk::message::*;
@@ -19,6 +22,18 @@ use vhostuser_rs::SlaveListener;
 
 const EVENT_SLAVE_IDX: u64 = 0;
 const EVENT_VUBMSG_IDX: u64 = 1;
+const EVENT_URING_IDX: u64 = 2;
+
+fn read_eventfd(efd: RawFd) {
+    let mut buf: u64 = 0;
+    unsafe {
+        libc::read(
+            efd,
+            &mut buf as *mut u64 as *mut libc::c_void,
+            mem::size_of::<u64>(),
+        )
+    };
+}
 
 enum VringWorkerMsg {
     Stop,
@@ -31,27 +46,106 @@ struct VringWorkerEndpoint {
 }
 
 struct VringWorker {
-    vring: Vring<StorageBackendRaw>,
+    vring: Arc<Mutex<Vring>>,
+    image_path: String,
+    async_backend: bool,
+    poll_ns: u128,
+    readonly: bool,
     eventfd: EventFd,
     receiver: Receiver<VringWorkerMsg>,
 }
 
 impl VringWorker {
     fn new(
-        vring: Vring<StorageBackendRaw>,
+        vring: Arc<Mutex<Vring>>,
+        image_path: String,
+        async_backend: bool,
+        poll_ns: u128,
+        readonly: bool,
         eventfd: EventFd,
         receiver: Receiver<VringWorkerMsg>,
     ) -> Self {
         VringWorker {
             vring,
+            image_path,
+            async_backend,
+            poll_ns,
+            readonly,
             eventfd,
             receiver,
         }
     }
 
+    fn poll_queues<S: StorageBackend>(&mut self, backend: &mut S) {
+        let mut vring = self.vring.lock().unwrap();
+
+        let mut start_time = Instant::now();
+        loop {
+            if vring.process_completions(backend).unwrap() || vring.process_queue(backend).unwrap()
+            {
+                match vring.signal_guest() {
+                    Ok(_) => (),
+                    Err(err) => error!("error signaling guest: {:?}", err),
+                }
+                start_time = Instant::now();
+            }
+
+            if self.poll_ns == 0
+                || Instant::now().duration_since(start_time).as_nanos() > self.poll_ns
+            {
+                break;
+            }
+        }
+    }
+
+    fn main_loop<S: StorageBackend>(
+        &mut self,
+        mut backend: S,
+        epoll_raw_fd: RawFd,
+        kick_fd: RawFd,
+        uring_efd: EventFd,
+    ) {
+        debug!("vring working entering loop");
+        loop {
+            const EPOLL_EVENTS_LEN: usize = 100;
+            let mut events = vec![epoll::Event::new(epoll::Events::empty(), 0); EPOLL_EVENTS_LEN];
+            let num_events = match epoll::wait(epoll_raw_fd, -1, &mut events[..]) {
+                Ok(events) => events,
+                Err(_) => continue,
+            };
+
+            for event in events.iter().take(num_events) {
+                let idx = event.data as u64;
+                match idx {
+                    EVENT_SLAVE_IDX => {
+                        read_eventfd(kick_fd);
+                        self.poll_queues(&mut backend);
+                    }
+                    EVENT_URING_IDX => {
+                        read_eventfd(uring_efd.as_raw_fd());
+                        self.poll_queues(&mut backend);
+                    }
+                    EVENT_VUBMSG_IDX => {
+                        self.eventfd.read().unwrap();
+                        match self.receiver.recv().unwrap() {
+                            VringWorkerMsg::Stop => {
+                                debug!("vring working stopping at main thread request");
+                                return ();
+                            }
+                        }
+                    }
+                    _ => {
+                        panic!("unknown index event!");
+                    }
+                }
+            }
+        }
+    }
+
     fn run(&mut self) {
+        let uring_efd = EventFd::new().expect("Can't create uring efd");
         let epoll_raw_fd = epoll::create(true).expect("Can't create epoll ofd");
-        let kick_fd = self.vring.get_kick_fd();
+        let kick_fd = self.vring.lock().unwrap().get_kick_fd();
 
         epoll::ctl(
             epoll_raw_fd,
@@ -69,49 +163,38 @@ impl VringWorker {
         )
         .unwrap();
 
-        debug!("vring working entering loop");
-        loop {
-            const EPOLL_EVENTS_LEN: usize = 100;
-            let mut events = vec![epoll::Event::new(epoll::Events::empty(), 0); EPOLL_EVENTS_LEN];
-            let num_events = match epoll::wait(epoll_raw_fd, -1, &mut events[..]) {
-                Ok(events) => events,
-                Err(_) => continue,
-            };
+        epoll::ctl(
+            epoll_raw_fd,
+            epoll::ControlOptions::EPOLL_CTL_ADD,
+            uring_efd.as_raw_fd(),
+            epoll::Event::new(epoll::Events::EPOLLIN, EVENT_URING_IDX),
+        )
+        .unwrap();
 
-            for event in events.iter().take(num_events) {
-                let idx = event.data as u64;
-                match idx {
-                    EVENT_SLAVE_IDX => {
-                        let mut buf: u64 = 0;
-                        unsafe {
-                            libc::read(
-                                kick_fd,
-                                &mut buf as *mut u64 as *mut libc::c_void,
-                                mem::size_of::<u64>(),
-                            )
-                        };
-
-                        if self.vring.process_queue().unwrap() {
-                            match self.vring.signal_guest() {
-                                Ok(_) => (),
-                                Err(err) => error!("error signaling guest: {:?}", err),
-                            }
-                        }
-                    }
-                    EVENT_VUBMSG_IDX => {
-                        self.eventfd.read().unwrap();
-                        match self.receiver.recv().unwrap() {
-                            VringWorkerMsg::Stop => {
-                                debug!("vring working stopping at main thread request");
-                                return ();
-                            }
-                        }
-                    }
-                    _ => {
-                        panic!("unknown index event!");
-                    }
+        if self.async_backend {
+            let storage_backend = match StorageBackendRawAsync::new(
+                &self.image_path,
+                uring_efd.as_raw_fd(),
+                self.readonly,
+                libc::O_DIRECT,
+            ) {
+                Ok(s) => s,
+                Err(e) => {
+                    error!("Can't open disk image {}: {}", self.image_path, e);
+                    exit(-1);
                 }
-            }
+            };
+            self.main_loop(storage_backend, epoll_raw_fd, kick_fd, uring_efd);
+        } else {
+            let storage_backend =
+                match StorageBackendRaw::new(&self.image_path, self.readonly, libc::O_DIRECT) {
+                    Ok(s) => s,
+                    Err(e) => {
+                        error!("Can't open disk image {}: {}", self.image_path, e);
+                        exit(-1);
+                    }
+                };
+            self.main_loop(storage_backend, epoll_raw_fd, kick_fd, uring_efd);
         }
     }
 }
@@ -139,6 +222,25 @@ fn main() {
                 .required(true)
                 .help("Use this raw image or block device as backend"),
         )
+        .arg(
+            Arg::with_name("async_backend")
+                .long("async_backend")
+                .short("a")
+                .help("Use an asynchronous backend storage (requires io_uring support)"),
+        )
+        .arg(
+            Arg::with_name("poll_ns")
+                .long("poll_ns")
+                .short("p")
+                .takes_value(true)
+                .help("Keep polling the queue for this amount of time (default: 32000)"),
+        )
+        .arg(
+            Arg::with_name("readonly")
+                .long("readonly")
+                .short("r")
+                .help("Open the storage backend in read-only mode"),
+        )
         .get_matches();
 
     let socket_path = cmd_args
@@ -148,7 +250,29 @@ fn main() {
         .value_of("backend")
         .expect("Can't parse backend path");
 
-    let storage_backend = match StorageBackendRaw::new(disk_image_path) {
+    let readonly;
+    if cmd_args.is_present("readonly") {
+        readonly = true;
+    } else {
+        readonly = false;
+    }
+
+    let async_backend;
+    if cmd_args.is_present("async_backend") {
+        async_backend = true;
+    } else {
+        async_backend = false;
+    }
+
+    let poll_ns;
+    if cmd_args.is_present("poll_ns") {
+        let poll_ns_str = cmd_args.value_of("poll_ns").expect("Invalid poll_ns value");
+        poll_ns = poll_ns_str.parse::<u128>().expect("Invalid poll_ns value");
+    } else {
+        poll_ns = 32000u128;
+    }
+
+    let storage_backend = match StorageBackendRaw::new(disk_image_path, true, 0) {
         Ok(s) => s,
         Err(e) => {
             error!("Can't open disk image {}: {}", disk_image_path, e);
@@ -212,14 +336,7 @@ fn main() {
                     }
                 },
                 EVENT_VUBMSG_IDX => {
-                    let mut buf: u64 = 0;
-                    unsafe {
-                        libc::read(
-                            main_eventfd.as_raw_fd(),
-                            &mut buf as *mut u64 as *mut libc::c_void,
-                            mem::size_of::<u64>(),
-                        )
-                    };
+                    read_eventfd(main_eventfd.as_raw_fd());
 
                     match main_receiver.recv().unwrap() {
                         VubMessage::EnableVring(msg) => {
@@ -231,8 +348,15 @@ fn main() {
                                 .unwrap()
                                 .get_vring(msg.index)
                                 .expect("Can't get vring");
-                            let mut worker =
-                                VringWorker::new(vring, eventfd.try_clone().unwrap(), receiver);
+                            let mut worker = VringWorker::new(
+                                vring,
+                                disk_image_path.to_string(),
+                                async_backend,
+                                poll_ns,
+                                readonly,
+                                eventfd.try_clone().unwrap(),
+                                receiver,
+                            );
 
                             let join_handle = thread::spawn(move || worker.run());
                             worker_endpoints.insert(
